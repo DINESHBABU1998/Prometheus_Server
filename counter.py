48a0e782d849769ef672c28fba0f90be38820381
ubuntuuser <ubuntuuser@Ubuntu.myguest.virtualbox.org>
2024-02-20 19:12:59 +0530

    second push

--- counter.py
+++ counter.py
@@ -1,5 +1,9 @@
 import http.server
-from prometheus_client import start_http_server
+import random
+from prometheus_client import start_http_server,Counter
+
+REQUEST_COUNT = Counter('app_requests_count','total http request count')
+RANDOM_COUNT = Counter('app_random_count','increment counter by random value')
 
 APP_PORT = 8000
 METRICS_PORT = 8001
@@ -7,6 +11,9 @@ METRICS_PORT = 8001
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        REQUEST_COUNT.inc()
+        random_val = random.random()*10
+        RANDOM_COUNT.inc(random_val)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
